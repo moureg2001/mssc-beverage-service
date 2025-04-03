e7d66c9765f5f2ed6b7126579c7d67192ad9cc84
firetor <moureg2001@gmail.com>
2025-04-04 01:28:51 +0200

    Implementing Beverage Controller. closes #3

---
+++ src/main/java/com/springframework/msscbeverageservice/web/controller/BeverageController.java
@@ -0,0 +1,37 @@
+package com.springframework.msscbeverageservice.web.controller;
+
+import com.springframework.msscbeverageservice.web.model.BeverageDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beverage")
+@RestController
+public class BeverageController {
+
+    @GetMapping("/{beverageId}")
+    public ResponseEntity<BeverageDto> getBeverageById(@PathVariable("beverageId") UUID beverageId) {
+        // TODO Impl
+        return new ResponseEntity<>(BeverageDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity saveNewBeverage(@RequestBody BeverageDto beverageDto) {
+        // TODO Impl
+        return new ResponseEntity<>(BeverageDto.builder().build(), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beverageId}")
+    public ResponseEntity updateBeverage(@PathVariable("beverageId") UUID beverageId, @RequestBody BeverageDto beverageDto) {
+        // TODO Impl
+        return new ResponseEntity<>(BeverageDto.builder().build(), HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{beverageId}")
+    public ResponseEntity deleteBeverage(@PathVariable("beverageId") UUID beverageId) {
+        // TODO Impl
+        return new ResponseEntity<>(BeverageDto.builder().build(), HttpStatus.NO_CONTENT);
+    }
+}
